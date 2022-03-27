a18f579c166aa676f22894aa7e308b3ea3433aa6
dprojects <dprojects@users.noreply.github.com>
2022-03-27 20:52:55 +0200

    more locations for URL

--- README.md
+++ README.md
@@ -2,10 +2,20 @@
 
 This is FreeCAD macro for woodworking to apply and store textures.
 
-Currently the URL to the texture can be stored at FreeCAD project at objects description (Label2). In the future this macro will be improved to get more possibilities to store URL (material card, dedicated object's property). Currently it is prototype state but is working fine and allow to store different textures from the internet for objects.
+# How to store texture URL
 
-![001](https://raw.githubusercontent.com/dprojects/setTextures/master/Screenshots/001.png)
+* At objects description `Label2`.
 
+	![001](https://raw.githubusercontent.com/dprojects/setTextures/master/Screenshots/001.png)
+
+* At `Texture` object property, You have to add it to the object on Your own, refer to FreeCAD documentation, to find out how to do it:
+	
+	![002](https://raw.githubusercontent.com/dprojects/setTextures/master/Screenshots/002.png)
+
+* At the `Material Card` for the `Material` applied to the object:
+
+	![003](https://raw.githubusercontent.com/dprojects/setTextures/master/Screenshots/003.png)
+	
 # License
 
 MIT
--- setTextures.py
+++ setTextures.py
@@ -2,46 +2,93 @@
 
 # FreeCAD macro for woodworking to apply and store textures
 # Author: Darek L (aka dprojects)
-# Version: 1.0 (total prototype)
+# Version: 2.0 (improved locations)
 # Latest version: https://github.com/dprojects/setTextures
 
-import FreeCADGui
-from PySide import QtGui
+import FreeCAD, FreeCADGui
+from PySide import QtGui, QtCore
 from pivy import coin
 import urllib.request
 import tempfile
 import os
 
-with tempfile.TemporaryDirectory() as tmpDir:
-	FreeCAD.Console.PrintMessage("\n Directory: "+tmpDir)
+empty = ""
 
-	#for obj in FreeCADGui.Selection.getSelection():
+# create temp directory
+with tempfile.TemporaryDirectory() as tmpDir:
+
+	FreeCAD.Console.PrintMessage("\n\n")
+	FreeCAD.Console.PrintMessage("Directory: "+tmpDir)
+
+	# search all objects
 	for obj in FreeCAD.activeDocument().Objects:
-	
-		if str(obj.Label2) == "":
+		
+		textureURL = ""
+
+		try:
+			
+			# support for texture URL stored at objects description
+			if textureURL == "":
+				ref = str(obj.Label2)
+				if ref != "" and ref.startswith("http"):
+					textureURL = ref
+
+			# support for texture URL stored at objects Texture property	
+			if textureURL == "":
+				ref = str(obj.Texture)
+				if ref != "" and ref.startswith("http"):
+					textureURL = ref
+
+			# support for texture URL stored at Material Card TexturePath
+			if textureURL == "":
+				ref = str(obj.Material.Material["TexturePath"])
+				if ref != "" and ref.startswith("http"):
+					textureURL = ref
+
+		except:
+			textureURL = ""
+
+		# if no texture found for object skip it
+		if str(textureURL) == "":
 			continue
 
-		textureURL = str(obj.Label2)
+		# mark found
+		empty = textureURL
+
+		# get image from URL
 		data = urllib.request.urlopen(textureURL)
 
+		# create temp file with image
 		texFilename = os.path.join(tmpDir, obj.Label)
 		out = open(str(texFilename), "wb")
 		out.write(data.read())
 		out.close()
-	
-		FreeCAD.Console.PrintMessage("\n File: "+texFilename)
 
+		FreeCAD.Console.PrintMessage("\n")	
+		FreeCAD.Console.PrintMessage("File: "+texFilename)
+
+		# apply texture
 		rootnode = obj.ViewObject.RootNode
 		texture =  coin.SoTexture2()
 		texture.filename = texFilename
 
+		# check if already texure is applied
 		skip = 0
 		for i in rootnode.getChildren():
 			if hasattr(i, "filename"):
+				
+				# replace texure
 				i.filename = ""
 				i.filename = texFilename
 				skip = 1
 
+		# set texture as new if not applied
 		if skip == 0:
 			rootnode.insertChild(texture, 1)
-				
+
+# if no textures show info
+if empty == ""	:
+	iText = ""
+	iText += "No textures URLs found. \n" 
+	iText += "Please see the documentation to find out how to store textures. \n"
+	QtGui.QMessageBox.information(None,"setTextures",str(iText))
